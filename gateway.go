bb23d68fbebfeb22e2bccda8631e275cd8dc3349
Seth Hoenig <seth.a.hoenig@gmail.com>
2014-01-16 21:17:00 -0600

    add cli args for mqtt broker settings

--- gateway.go
+++ gateway.go
@@ -11,11 +11,11 @@ import (
 
 func main() {
 	stopsig := registerSignals()
-	aggregating, mqttBroker, port := setup()
+	aggregating, port, mqttopts := setup()
 	var gateway SN.Gateway
 	if aggregating {
 		fmt.Println("GNATT Gateway starting in aggregating mode")
-		gateway = initAggregating(mqttBroker, stopsig, port)
+		gateway = initAggregating(port, stopsig, mqttopts)
 	} else {
 		fmt.Println("GNATT Transparent gateway not yet implemented")
 		os.Exit(0)
@@ -26,24 +26,35 @@ func main() {
 	gateway.Start()
 }
 
-func setup() (bool, string, int) {
+func setup() (bool, int, [4]string) {
 	var aggregating bool
-	var broker string
 	var udpport int
+	var mqttbroker string
+	var mqttuser string
+	var mqttpass string
+	var mqttcid string
+
 	flag.BoolVar(&aggregating, "aggregating", false, "Transparent or Aggregating")
-	flag.StringVar(&broker, "broker", "", "MQTT Broker URI")
 	flag.IntVar(&udpport, "port", 0, "MQTT-SN UDP Listening Port")
+	flag.StringVar(&mqttbroker, "mqtt-broker", "", "MQTT Broker URI")
+	flag.StringVar(&mqttuser, "mqtt-user", "", "MQTT User")
+	flag.StringVar(&mqttpass, "mqtt-password", "", "MQTT Password")
+	flag.StringVar(&mqttcid, "mqtt-clientid", "gnatt-gateway", "MQTT Client ID")
+
 	flag.Parse()
-	if broker == "" {
-		fmt.Println("Must specify -broker <tcp://host:port>")
+	if mqttbroker == "" {
+		fmt.Println("Must specify -mqtt-broker <tcp://host:port>")
 		os.Exit(1)
 	}
+
 	if udpport == 0 {
 		fmt.Println("Must specify -port <port>")
 		os.Exit(1)
 	}
 
-	return aggregating, broker, udpport
+	opts := [4]string{mqttbroker, mqttuser, mqttpass, mqttcid}
+
+	return aggregating, udpport, opts
 }
 
 func registerSignals() chan os.Signal {
@@ -52,9 +63,18 @@ func registerSignals() chan os.Signal {
 	return c
 }
 
-func initAggregating(broker string, stopsig chan os.Signal, port int) *SN.AggGate {
+func initAggregating(port int, stopsig chan os.Signal, mqttopts [4]string) *SN.AggGate {
 	opts := MQTT.NewClientOptions()
-	opts.SetBroker(broker)
+	opts.SetBroker(mqttopts[0])
+	if mqttopts[1] != "" {
+		opts.SetUsername(mqttopts[1])
+	}
+	if mqttopts[2] != "" {
+		opts.SetPassword(mqttopts[2])
+	}
+	if mqttopts[3] != "" {
+		opts.SetClientId(mqttopts[3])
+	}
 	ag := SN.NewAggGate(opts, stopsig, port)
 	return ag
 }
